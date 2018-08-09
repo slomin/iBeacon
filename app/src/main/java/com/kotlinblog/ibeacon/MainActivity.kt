5d3654e5ead360a9533a48f4d6a3d1bd774098c2
Jan Slominski <jan.slominski@dyson.com>
2018-08-09 14:10:39 +0100

    (+) Adding AltBeacon library, Adding COARSE_LOCATION permission request code

--- app/src/main/java/com/kotlinblog/ibeacon/MainActivity.kt
+++ app/src/main/java/com/kotlinblog/ibeacon/MainActivity.kt
@@ -1,12 +1,52 @@
 package com.kotlinblog.ibeacon
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkBluetoothPermissions()
+    }
+
+    private fun checkBluetoothPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                // Ask another time here (the same ask dialog for now)
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION)
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION)
+            }
+        } else {
+            Log.d("Janek", "Permission already granted")
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    Log.d("Janek", "Permission GRANTED!!!")
+                } else {
+                    Toast.makeText(this, "Please grant LOCATION permission to use this app", Toast.LENGTH_LONG).show()
+                    finish()
+                }
+                return
+            }
+            else -> {
+
+            }
+        }
+    }
+
+    companion object {
+        const val MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 0
     }
 }
